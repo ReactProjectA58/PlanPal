075316a5f89f41d8ce76f5c2d65be0179db54b89
KaloyanKostov <kostov.kaloyan44@gmail.com>
2024-05-22 01:36:39 +0300

    Improved Contact Search

--- src/common/helpers/icons.jsx
+++ src/common/helpers/icons.jsx
@@ -2,12 +2,14 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import {
   faAddressCard,
   faBars,
+  faChevronLeft,
   faListCheck,
   faMagnifyingGlass,
   faMoon,
   faPhone,
   faSun,
   faTimes,
+  faUserGroup,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -56,7 +58,7 @@ export const Moon = () => {
     <FontAwesomeIcon
       icon={faMoon}
       className="swap-off fill-current w-10 h-10"
-      style={{ width: "24px", height: "24px" }}
+      style={{ width: "20px", height: "20px" }}
     />
   );
 };
@@ -66,7 +68,15 @@ export const Sun = () => {
     <FontAwesomeIcon
       icon={faSun}
       className="swap-on fill-current w-10 h-10"
-      style={{ width: "24px", height: "24px" }}
+      style={{ width: "20px", height: "20px" }}
     />
   );
+};
+
+export const Contacts = () => {
+  return <FontAwesomeIcon icon={faChevronLeft} size={"lg"} />;
+};
+
+export const GoBack = () => {
+  return <FontAwesomeIcon icon={faUserGroup} size={"lg"} />;
 };
--- src/components/Contacts/ContactsList.jsx
+++ src/components/Contacts/ContactsList.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState, useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { searchUsers } from "../../services/search.service";
+import { Contacts, GoBack } from "../../common/helpers/icons";
 import SearchBar from "../SearchBar/SearchBar";
 
 export default function ContactsList() {
   const [searchResults, setSearchResults] = useState([]);
+  const [currentView, setCurrentView] = useState("My Contacts");
+  const [clearSearch, setClearSearch] = useState(false);
   const [isSearching, setIsSearching] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -16,6 +20,7 @@ export default function ContactsList() {
       searchUsers(queryParam)
         .then((userResults) => {
           setSearchResults(userResults);
+          setSearchQuery(queryParam);
         })
         .catch((error) => console.error("Error:", error));
     }
@@ -24,20 +29,43 @@ export default function ContactsList() {
   const getContacts = () => {
     if (isSearching) {
       return searchResults;
-    } else {
-      return [];
     }
+    return [];
   };
 
   const handleSearch = (query) => {
     navigate(`/contacts?query=${query}`);
   };
 
+  const handleContactsBackClick = () => {
+    navigate("/contacts");
+    setCurrentView("My Contacts");
+    setClearSearch((prev) => !prev);
+  };
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-[4fr_2fr] gap-6">
       <div className="flex flex-col">
         <div className="flex justify-between items-center ml-5 mb-6 flex-col md:flex-row">
-          <SearchBar onSearch={handleSearch} />
+          <div className="flex items-center justify-between mb-4 md:mb-0">
+            {isSearching ? (
+              <a onClick={handleContactsBackClick}>
+                <Contacts />
+              </a>
+            ) : (
+              <GoBack />
+            )}
+            <h2 className="text-lg md:ml-4">
+              {isSearching
+                ? `Contacts Found for '${searchQuery}'`
+                : currentView}
+            </h2>
+          </div>
+          <SearchBar
+            onSearch={handleSearch}
+            currentView={currentView}
+            clearSearch={clearSearch}
+          />
         </div>
         <div className="card-container">
           <ul>
@@ -50,14 +78,17 @@ export default function ContactsList() {
                   <div className="text-lg font-semibold">
                     {contact.firstName} {contact.lastName}
                   </div>
-                  <div className="text-sm text-gray-600">
-                    <strong>Username:</strong> {contact.handle}
+                  <div className="text-sm">
+                    <strong className="text-gray-600">Username:</strong>{" "}
+                    {contact.handle}
                   </div>
-                  <div className="text-sm text-gray-600">
-                    <strong>Email:</strong> {contact.email}
+                  <div className="text-sm">
+                    <strong className="text-gray-600">Email:</strong>{" "}
+                    {contact.email}
                   </div>
-                  <div className="text-sm text-gray-600">
-                    <strong>Phone:</strong> {contact.phoneNumber}
+                  <div className="text-sm">
+                    <strong className="text-gray-600">Phone:</strong>{" "}
+                    {contact.phoneNumber}
                   </div>
                 </div>
               </li>
