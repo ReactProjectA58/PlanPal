5d6b00f429f1e05036697fc50cb37d80ba084e15
KaloyanKostov <kostov.kaloyan44@gmail.com>
2024-06-05 21:02:13 +0300

    Restyle create event form

--- src/components/Events/CreateEventForm.jsx
+++ src/components/Events/CreateEventForm.jsx
@@ -1,16 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import { useContext, useEffect, useRef, useState } from "react";
-import { addEvent, getUserContacts, inviteUser } from "../../services/event.service.js";
+import { addEvent, getUserContacts } from "../../services/event.service.js";
 import { uploadCover } from "../../services/upload.service.js";
 import Button from "../Button.jsx";
 import { AppContext } from "../../context/AppContext.jsx";
-import { validateTitle, validateDescription, validateLocation, validateStartDate, validateEndDate, validateStartTime, validateEndTime } from "../../common/helpers/validationHelpers.js";
-import { GoBackArrow } from "../../common/helpers/icons.jsx";
+import {
+  validateTitle,
+  validateDescription,
+  validateLocation,
+  validateStartDate,
+  validateEndDate,
+  validateStartTime,
+  validateEndTime,
+} from "../../common/helpers/validationHelpers.js";
+import { ArrowDown, GoBackArrow } from "../../common/helpers/icons.jsx";
 import {
   EVENT_SPORTS_COVER,
   EVENT_ENTERTAINMENT_COVER,
   EVENT_CULTURE_AND_SCIENCE_COVER,
-  EVENT_COVER_BY_DEFAULT
+  EVENT_COVER_BY_DEFAULT,
 } from "../../common/constants.js";
 
 export default function CreateEvent() {
@@ -24,14 +32,42 @@ export default function CreateEvent() {
     description: "",
     isPublic: false,
     isReoccurring: "never",
-    category: "Entertainment", // Added category field
+    category: "Entertainment",
   });
   const [coverFile, setCoverFile] = useState(null);
   const [errors, setErrors] = useState({});
   const [contacts, setContacts] = useState([]);
+  const [description, setDescription] = useState("");
   const { userData } = useContext(AppContext);
   const navigate = useNavigate();
-  const inviteRef = useRef(null);
+
+  const reoccurringRef = useRef(null);
+  const categoryRef = useRef(null);
+  const [isReoccurringOpen, setIsReoccurringOpen] = useState(false);
+  const [selectedReoccurringOption, setSelectedReoccurringOption] =
+    useState(false);
+  const [selectedCategoryOption, setSelectedCategoryOption] = useState(null);
+  const [isCategoryOpen, setIsCategoryOpen] = useState(false);
+  const [coverPreview, setCoverPreview] = useState(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        reoccurringRef.current &&
+        !reoccurringRef.current.contains(event.target)
+      ) {
+        setIsReoccurringOpen(false);
+      }
+      if (categoryRef.current && !categoryRef.current.contains(event.target)) {
+        setIsCategoryOpen(false);
+      }
+    }
+
+    window.addEventListener("click", handleClickOutside);
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [reoccurringRef, categoryRef]);
 
   useEffect(() => {
     const fetchContacts = async () => {
@@ -47,13 +83,36 @@ export default function CreateEvent() {
   const updateEvent = (value, key) => {
     setEvent({
       ...event,
-      [key]: value,
+      [key]: key === "description" ? description : value,
     });
   };
 
   const handleFileChange = (e) => {
-    setCoverFile(e.target.files[0]);
+    const file = e.target.files[0];
+    setCoverFile(file);
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setCoverPreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
   };
+
+  const recurrenceOptions = [
+    { value: "never", label: "Never" },
+    { value: "weekly", label: "Weekly" },
+    { value: "monthly", label: "Monthly" },
+    { value: "yearly", label: "Yearly" },
+    { value: "indefinitely", label: "Indefinitely" },
+  ];
+
+  const categoryOptions = [
+    { value: "Entertainment", label: "Entertainment" },
+    { value: "Sports", label: "Sports" },
+    { value: "Culture & Science", label: "Culture & Science" },
+    { value: "Other", label: "Other" },
+  ];
 
   const getDefaultCoverByCategory = (category) => {
     switch (category) {
@@ -69,7 +128,16 @@ export default function CreateEvent() {
   };
 
   const createEvent = async () => {
-    const { title, description, location, startDate, startTime, endDate, endTime, category } = event;
+    const {
+      title,
+      description,
+      location,
+      startDate,
+      startTime,
+      endDate,
+      endTime,
+      category,
+    } = event;
 
     const validationErrors = {
       title: validateTitle(title),
@@ -115,7 +183,7 @@ export default function CreateEvent() {
         description: "",
         isPublic: false,
         isReoccurring: "never",
-        category: "Entertainment", // Reset category field
+        category: "Entertainment",
       });
       setCoverFile(null);
 
@@ -127,106 +195,214 @@ export default function CreateEvent() {
   };
 
   return (
-    <div className="outer-create-event-container">
-      <div className="inner-create-event-container">
-        <div className="flex justify-between items-center mb-4">
-          <h1>Create Event</h1>
-          <GoBackArrow onClick={() => navigate("/events")} />
-        </div>
+    <div className="p-4 max-w-3xl mx-auto shadow-md rounded-lg">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl font-bold">Create Event</h1>
+        <GoBackArrow onClick={() => navigate("/events")} />
+      </div>
+
+      <div className="flex flex-wrap -mx-2">
         {[
           { label: "Title", key: "title", type: "text" },
+          { label: "Location", key: "location", type: "text" },
+        ].map(({ label, key, type }) => (
+          <div key={key} className="mb-4 px-2 w-full sm:w-1/2">
+            <label
+              htmlFor={`input-${key}`}
+              className="block text-sm font-medium"
+            >
+              {label} <span className="text-red-500">*</span>:
+            </label>
+            <div className=" mt-1">
+              <input
+                type={type}
+                value={event[key]}
+                onChange={(e) => updateEvent(e.target.value, key)}
+                name={`input-${key}`}
+                id={`input-${key}`}
+                className="shadow-sm  block w-full sm:text-sm rounded-md"
+              />
+              {errors[key] && (
+                <div className="text-red-500 text-sm">{errors[key]}</div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-wrap -mx-2">
+        {[
           { label: "Start Date", key: "startDate", type: "date" },
           { label: "Start Time", key: "startTime", type: "time" },
           { label: "End Date", key: "endDate", type: "date" },
           { label: "End Time", key: "endTime", type: "time" },
-          { label: "Location", key: "location", type: "text" },
-          { label: "Description", key: "description", type: "textarea" },
         ].map(({ label, key, type }) => (
-          <div key={key} className={`event-${key}`}>
-            <label htmlFor={`input-${key}`}>{label}:</label>
-            <div className="input-field">
-              {type === "textarea" ? (
-                <textarea
-                  className={`${key}-input-style`}
-                  value={event[key]}
-                  onChange={(e) => updateEvent(e.target.value, key)}
-                  name={`input-${key}`}
-                  id={`input-${key}`}
-                  cols="30"
-                  rows="10"
-                  style={{ boxShadow: "inset 0px 0px 5px rgba(0, 0, 0, 0.5)" }}
-                ></textarea>
-              ) : (
-                <input
-                  className={`${key}-input-style`}
-                  type={type}
-                  value={event[key]}
-                  onChange={(e) => updateEvent(e.target.value, key)}
-                  name={`input-${key}`}
-                  id={`input-${key}`}
-                  style={{ boxShadow: "inset 0px 0px 5px rgba(0, 0, 0, 0.5)" }}
-                />
+          <div key={key} className="mb-4 px-2 w-full sm:w-1/2">
+            <label
+              htmlFor={`input-${key}`}
+              className="block text-sm font-medium"
+            >
+              {label} <span className="text-red-500">*</span>:
+            </label>
+            <div className=" mt-1">
+              <input
+                type={type}
+                value={event[key]}
+                onChange={(e) => updateEvent(e.target.value, key)}
+                name={`input-${key}`}
+                id={`input-${key}`}
+                className="shadow-sm  block w-full sm:text-sm rounded-md"
+              />
+              {errors[key] && (
+                <div className="text-red-500 text-sm">{errors[key]}</div>
               )}
-              {errors[key] && <div className="error-message">{errors[key]}</div>}
             </div>
           </div>
         ))}
+      </div>
 
-        <div className="event-isPublic">
-          <label>
-            Public Event:
-            <input
-              type="checkbox"
-              checked={event.isPublic}
-              onChange={(e) => updateEvent(e.target.checked, "isPublic")}
-            />
-          </label>
-        </div>
-        <div className="event-reoccurring-option">
-          <label htmlFor="reoccurring-option">Reoccurring:</label>
-          <select
-            id="reoccurring-option"
-            value={event.isReoccurring}
-            onChange={(e) => updateEvent(e.target.value, "isReoccurring")}
-            className="reoccurring-option-style"
-          >
-            <option value="Weekly">Weekly</option>
-            <option value="Monthly">Monthly</option>
-            <option value="Yearly">Yearly</option>
-            <option value="Indefinitely">Indefinitely</option>
-            <option value="Never">Never</option>
-          </select>
-        </div>
-        <div className="event-category">
-          <label htmlFor="category">Category:</label>
-          <select
-            id="category"
-            value={event.category}
-            onChange={(e) => updateEvent(e.target.value, "category")}
-            className="category-option-style"
-          >
-            <option value="Entertainment">Entertainment</option>
-            <option value="Sports">Sports</option>
-            <option value="Culture & Science">Culture & Science</option>
-            <option value="Other">Other</option>
-          </select>
-        </div>
+      <div className="mb-4">
+        <label
+          htmlFor="input-description"
+          className="block text-sm font-medium "
+        >
+          Description:
+        </label>
+        <div className="mt-1">
+          <textarea className="textarea textarea-bordered textarea-sm w-full"></textarea>
 
-        <div className="flex items-center space-x-4">
-          <Button className="create-button-style" onClick={createEvent}>
-            Create
-          </Button>
-          <Button className="upload-button-style" onClick={() => document.getElementById('cover-upload').click()}>
-            Upload Cover
-          </Button>
+          {errors.description && (
+            <div className="text-red-500 text-sm">{errors.description}</div>
+          )}
+        </div>
+      </div>
+      <div className="mb-4">
+        <label className="block text-sm font-medium ">Public Event:</label>
+        <div className="mt-1">
           <input
-            type="file"
-            id="cover-upload"
-            style={{ display: 'none' }}
-            accept="image/*"
-            onChange={handleFileChange}
+            type="checkbox"
+            checked={event.isPublic}
+            onChange={(e) => updateEvent(e.target.checked, "isPublic")}
+            className="h-4 w-4   rounded"
           />
         </div>
+      </div>
+
+      <div className="mb-4" ref={reoccurringRef}>
+        <label
+          htmlFor="reoccurring-option"
+          className="block text-sm font-medium"
+        >
+          Reoccurring:
+        </label>
+        <div className="mt-1 relative">
+          <button
+            type="button"
+            className="w-full rounded-md shadow-sm pl-3 pr-10 py-2 text-left   sm:text-sm"
+            onClick={() => setIsReoccurringOpen(!isReoccurringOpen)}
+          >
+            <span className="block truncate">
+              {selectedReoccurringOption
+                ? selectedReoccurringOption.label
+                : "Select Occurrence"}
+            </span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+              <ArrowDown />
+            </span>
+          </button>
+          {isReoccurringOpen && (
+            <div className="origin-top-right absolute z-10 mt-1 w-full rounded-md glass bg-transparent ring-1 ring-black ring-opacity-5">
+              {recurrenceOptions.map((option) => (
+                <div
+                  key={option.value}
+                  className="cursor-pointer select-none relative py-2 pl-3 pr-9 hover:glass"
+                  onClick={() => {
+                    setSelectedReoccurringOption(option);
+                    setIsReoccurringOpen(false);
+                    updateEvent(option.value, "isReoccurring");
+                  }}
+                >
+                  <span className="block truncate">{option.label}</span>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="mb-4 relative" ref={categoryRef}>
+        <label htmlFor="category" className="block text-sm font-medium ">
+          Category:
+        </label>
+        <div className="mt-1 relative">
+          <button
+            type="button"
+            className="w-full rounded-md shadow-sm pl-3 pr-10 py-2 text-left focus:outline-none sm:text-sm"
+            id="category"
+            onClick={() => setIsCategoryOpen(!isCategoryOpen)}
+          >
+            <span className="block truncate">
+              {selectedCategoryOption
+                ? selectedCategoryOption.label
+                : "Select Category"}
+            </span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+              <ArrowDown />
+            </span>
+          </button>
+          {isCategoryOpen && (
+            <div className="origin-top-right absolute z-10 mt-1 w-full rounded-md glass bg-transparent ring-1 ring-black ring-opacity-5">
+              {categoryOptions.map((option) => (
+                <div
+                  key={option.value}
+                  className="cursor-pointer select-none relative py-2 pl-3 pr-9 hover:glass"
+                  onClick={() => {
+                    setSelectedCategoryOption(option);
+                    setIsCategoryOpen(false);
+                    updateEvent(option.value, "category");
+                  }}
+                >
+                  <span className="block truncate">{option.label}</span>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div>
+        {coverPreview && (
+          <img
+            src={coverPreview}
+            alt="Cover Preview"
+            className="w-96 h-64 object-cover rounded mb-2"
+          />
+        )}
+      </div>
+
+      <div className="flex items-center gap-8">
+        <Button className="font-bold py-2 px-4 rounded" onClick={createEvent}>
+          Create
+        </Button>
+        <Button
+          onClick={() => document.getElementById("cover-upload").click()}
+          className="font-bold py-2 px-4 rounded"
+        >
+          Upload Cover
+        </Button>
+        <Button
+          className="font-bold py-2 px-4 rounded"
+          onClick={() => console.log("Invite user clicked")}
+        >
+          Invite Contact
+        </Button>
+        <input
+          type="file"
+          id="cover-upload"
+          style={{ display: "none" }}
+          accept="image/*"
+          onChange={handleFileChange}
+        />
       </div>
     </div>
   );
